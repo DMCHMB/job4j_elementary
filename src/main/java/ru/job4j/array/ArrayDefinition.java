81114520f080f4469a36072bc0cd949a5ab1d011
Gromov Ilya <109763246+DMCHMB@users.noreply.github.com>
2023-07-05 22:26:05 +0300

    6.0.3. Заполнение массива. [#226300] Исправил под условия: убрал лишнее, убрал цикл

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -2,22 +2,15 @@ package ru.job4j.array;
 
 public class ArrayDefinition {
     public static void main(String[] args) {
-        short[] ages = new short[10];
-        String[] surnames = new String[100500];
-        float[] prices = new float[40];
         String[] names = new String[4];
-
         names[0] = "Petr Arsentev";
         names[1] = "Ivan Ivanov";
         names[2] = "John Smith";
         names[3] = "Hans Gruber";
 
-        for (int i = 0; i < names.length; i++) {
-            System.out.println(names[i]);
-        }
-
-        System.out.println(ages.length);
-        System.out.println(surnames.length);
-        System.out.println(prices.length);
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
